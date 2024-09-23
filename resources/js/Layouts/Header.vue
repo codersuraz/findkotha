d737dbdbbba6aa9e6ed39cd173db13facf0cdecd
Surya Prasad Khanal <codersuraz@gmail.com>
2024-09-23 20:58:01 +0545

    WIP Language Switcher

--- resources/js/Layouts/Header.vue
+++ resources/js/Layouts/Header.vue
@@ -1,5 +1,9 @@
 <script setup>
     import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+    import Dropdown from '@/Components/Dropdown.vue';
+    import Globe from '@/Icons/Globe.vue';
+    import NepalFlag from '@/Icons/NepalFlag.vue';
+    import UsFlag from '@/Icons/UsFlag.vue';
     import { Link } from '@inertiajs/vue3';
 </script>
 
@@ -10,7 +14,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0">
                     <Link href="/">
-                    <ApplicationLogo class="w-32 text-[#e83d5f]" />
+                        <ApplicationLogo class="w-32 text-[#e83d5f]" />
                     </Link>
                 </div>
 
@@ -41,31 +45,41 @@
                 </div>
 
                 <!-- Navigation -->
-                <nav class="flex items-center space-x-4">
+                <nav class="flex items-center space-x-1">
                     <a
                         href="#"
-                        class="text-sm font-medium text-gray-700 hover:text-gray-900"
+                        class="inline-flex items-center px-4 py-3 h-10 text-sm font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-100 rounded-full"
                     >List your Room</a>
-                    <button
-                        href="#"
-                        class="inline-grid place-items-center text-sm font-medium text-gray-700 w-10 h-10 rounded-full transition duration-100 hover:text-gray-900 hover:bg-gray-100"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            class="size-6"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
-                            />
-                        </svg>
 
-                    </button>
+                    <!-- Language Switcher -->
+                    <Dropdown>
+                        <template #trigger>
+                            <button
+                                class="inline-grid place-items-center text-sm font-medium text-gray-700 w-10 h-10 rounded-full transition duration-100 hover:text-gray-900 hover:bg-gray-100"
+                            >
+                                <Globe />
+                            </button>
+                        </template>
+                        <template #content>
+                            <div class="py-1">
+                                <a
+                                    href="#"
+                                    class="flex items-center gap-0.5 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                >
+                                    <NepalFlag class="w-5"/>
+                                    नेपाली
+                                </a>
+                                <a
+                                    href="#"
+                                    class="flex items-center gap-1 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                >
+                                    <UsFlag class="w-4"/>
+                                    English
+                                </a>
+                            </div>
+                        </template>
+                    </Dropdown>
+
                     <div
                         class="relative"
                         v-if="$page.props.auth?.user"
@@ -81,15 +95,16 @@
                             >
                         </button>
                     </div>
-                    <div class="relative flex items-center gap-4">
+                    <div class="relative flex items-center gap-1">
                         <button
                             class="flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 focus:outline-none focus:text-gray-900"
                         >
                             <span class="sr-only">Open signup menu</span>
                             <Link
                                 href="/register"
-                                class="text-sm font-medium text-gray-700 hover:text-gray-900"
-                            >Signup</Link>
+                                class="text-sm font-medium text-gray-700 hover:text-gray-900 inline-flex items-center px-4 py-3 h-10 hover:bg-gray-100 rounded-full"
+                            >
+                            Signup</Link>
                         </button>
                         <button
                             class="flex items-center text-sm font-medium text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:bg-red-600 rounded-full px-4 py-2"
